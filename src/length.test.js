b1c617d19bde3177be6ce6b5202a5082c95175d4
Enoh <eno.col@gmail.com>
2023-08-30 18:32:31 +0100

    join both functions and put them in describe jest method

--- src/length.test.js
+++ src/length.test.js
@@ -1,6 +1,13 @@
 const stringLength = require('../src/length');
+const reversed = require('../src/reversed');
 
-const string = '';
-test('return the length of a string', () => {
-  expect(() => stringLength(string)).toThrow(Error);
+describe('to test both funtions', () => {
+  const string = 'this is me';
+  test('return the length of a string', () => {
+    expect(stringLength(string)).toBe(10);
+  });
+
+  test('to return a reversed string', () => {
+    expect(reversed(string)).toMatch(/e m   s i   s i h t/);
+  });
 });
--- src/reversed.test.js
+++
@@ -1,7 +0,0 @@
-//import reversed from './reversed';
-const reversed = require('../src/reversed');
-const string = 'this is me';
-
-test('to return a reversed string', () => {
-  expect(reversed(string)).toMatch(/e m   s i   s i h t/);
-});
